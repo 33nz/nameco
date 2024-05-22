33deabd2ae605d0322c596697de23ef6d62ab1e1
Jacob Wingate <jacob.wingate33@gmail.com>
2024-05-22 23:19:27 +1200

    add tailwind set-up and associated word interface

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,4 +1,5 @@
 import { useFruits } from '../hooks/useFruits.ts'
+import '../styles/index.css'
 
 function App() {
   const { data } = useFruits()
@@ -6,7 +7,9 @@ function App() {
   return (
     <>
       <div className="app">
-        <h1>Fullstack Boilerplate - with Fruits!</h1>
+        <h1 className="text-9xl text-gray-900 dark:text-white bg-blue-600">
+          Nameco.io
+        </h1>
         <ul>{data && data.map((fruit) => <li key={fruit}>{fruit}</li>)}</ul>
       </div>
     </>
